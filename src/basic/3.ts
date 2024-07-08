2a33343e58ddd431d295a11a31835deccb8881e8
AlenaDenysenko <naopiya28@gmai.com>
2024-07-08 12:17:27 +0300

    TS HW01

---
+++ src/basic/3.ts
@@ -0,0 +1,9 @@
+let unionType: string | number;
+unionType = 'hello';
+unionType = 123;
+
+type Status = 'enable' | 'disable';
+let status: Status;
+status = 'enable';
+status = 'disable';
+
---
+++ src/generics/2.ts
@@ -0,0 +1,13 @@
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+}
+
+function compare<T extends keyof AllType>(top: Pick<AllType, T>, bottom: Pick<AllType, T>): Pick<AllType, T> {
+  return {
+    ...top,
+    ...bottom
+  };
+}
